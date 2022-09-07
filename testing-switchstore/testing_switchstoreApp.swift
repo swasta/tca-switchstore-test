1a5e54e3319f4ffc13fc7ae9ec6efaeaac89b0b9
Nikita Borodulin <link.bor@gmail.com>
2022-09-07 14:56:34 +0400

    Initial Commit

---
+++ testing-switchstore/testing_switchstoreApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Nikita Borodulin on 07.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct testing_switchstoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
